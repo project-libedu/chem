c51c8921d507fc49252d5980ac9d6c3134ca962c
Jan Palma <60009551+mobilex1122@users.noreply.github.com>
2023-09-26 14:39:16 +0200

    Added perigen

--- js/index.js
+++ js/index.js
@@ -1,5 +1,30 @@
-/** @type {import("../lib/perigen").PeriGen} */
-import { PeriGen } from "../lib/perigen.js"
-const perigen = new PeriGen("#a")
+import { PeriGen } from "../lib/perigen/main.js"
+const perigen = new PeriGen("#periodictable-select")
+const GenerateTable = async () => {
+	// Source: https://github.com/Bowserinator/Periodic-Table-JSON
+	const dataRaw = await fetch(
+		"https://raw.githubusercontent.com/Bowserinator/Periodic-Table-JSON/master/PeriodicTableJSON.json"
+	)
+	perigen.setDataSource(await dataRaw.json())
 
-console.log(perigen)
+	perigen.generate(true)
+}
+
+// eslint-disable-next-line no-unused-vars
+function getData() {
+	const data = perigen.getSelected("#periodictable-select")
+	let qviz = []
+	data.forEach((elem) => {
+		qviz.push({
+			symbol: perigen.getElement(elem).symbol,
+			name: perigen.getElement(elem).name,
+		})
+	})
+	sessionStorage.setItem("qviz", "")
+
+	sessionStorage.setItem("qviz", JSON.stringify(qviz))
+}
+
+document.getElementById("getdata").addEventListener("click", getData)
+
+GenerateTable()
