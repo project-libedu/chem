a58ed8eb1fddbc4ba5c2f1d802abd4acf6cb244c
Jan Palma <60009551+mobilex1122@users.noreply.github.com>
2023-09-26 14:39:44 +0200

    Set prettier correctly

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,13 @@
 /** @type {import("prettier").Config} */
 const config = {
-    trailingComma: "es5",
-    tabWidth: 4,
-    semi: false,
-    singleQuote: false,
-    useTabs: true,
+	trailingComma: "es5",
+	tabWidth: 4,
+	semi: false,
+	singleQuote: false,
+	jsxSingleQuote: false,
+	jsxBracketSameLine: false,
+	useTabs: true,
+	quoteProps: "as-needed",
 }
 
 module.exports = config
